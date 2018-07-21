e466891f724502006242e39a889b683c12661a7f
ago.er@yandex.com <erajjo@yandex.com>
2018-07-21 13:15:38 -0400

    Created classes creditcard, customer, employee, location, reserves, transactions. Added properties to most and created a constructor for most.

---
+++ SecureX/SecureX/Account.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SecureXLibrary
+{
+    class Account
+    {
+        public Account(decimal funds, int currentDebt, char accountType)
+        {
+            Funds = funds;
+            CurrentDebt = currentDebt;
+            AccountType = accountType;
+        }
+
+        private decimal Funds { get; set; }
+        private int CurrentDebt { get; set; }
+        private char AccountType { get; set; }
+
+    }
+}
---
+++ SecureX/SecureX/Reserves.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SecureXLibrary
+{
+    class Reserves
+    {
+        public Reserves(int funds)
+        {
+            Funds = funds;
+        }
+
+        private int Funds { get; set; } = 1000000; //one million
+    }
+}
