aa756d6885baf8d984eaf0006f96b0c77d10a4b1
Chris Coronado <cbcoronado@live.com>
2018-07-25 13:43:10 -0400

    Added ChargeCredit and PayBalance methods to CreditCard library class

--- SecureX/SecureX/CreditCard.cs
+++ SecureX/SecureX/CreditCard.cs
@@ -11,5 +11,26 @@ namespace SecureXLibrary
         public decimal CurrentDebt { get; set; }
         public int CreditCardNumber { get; set; }
         public int CustomerId { get; set; }
+
+        // methods
+        public void ChargeCredit(decimal amount)
+        {
+            CreditLimit -= amount;
+            CurrentDebt += amount;
+        }
+
+        public void PayBalance(decimal amount)
+        {
+            if (amount > CurrentDebt) // check if paid amount is larger than the debt balance
+            {
+                amount -= CurrentDebt; // deduct debt from amount
+                CurrentDebt = 0.00m; // clear debt balance
+                CreditLimit += amount; // add excess amount to credit limit                
+            }
+            else
+            {
+                CurrentDebt -= amount;
+            }            
+        }
     }
 }
