fbd6f957046bcef3d7edcd321b350b39805475f5
Chris Coronado <cbcoronado@live.com>
2018-07-27 11:09:32 -0400

    Code Smell: changed mapper to static

--- SecureX/SecureX/Mapper.cs
+++ SecureX/SecureX/Mapper.cs
@@ -5,7 +5,7 @@ using System.Text;
 
 namespace SecureXLibrary
 {
-    public class Mapper
+    public static class Mapper
     {
         public static Account Map(SecureXContext.Account account) => new Account
         {
