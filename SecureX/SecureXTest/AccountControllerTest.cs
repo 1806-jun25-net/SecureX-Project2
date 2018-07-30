ed4c1b78b6a11c7dbae5cdc1f30096e2d0d536b7
Eric Agocs <ago.er@yandex.com>
2018-07-30 15:59:47 -0400

    uncommented stuff

--- SecureX/SecureXTest/AccountControllerTest.cs
+++ SecureX/SecureXTest/AccountControllerTest.cs
@@ -68,31 +68,31 @@ namespace SecureXTest
 
 
 
-        //[Fact]
-        //[CustomAssertion]
-        //public async void ShouldDelete_Account()
-        //{
+        [Fact]
+        [CustomAssertion]
+        public async void ShouldDelete_Account()
+        {
 
-        //    Mock<ISecureXRepository> MoqRepo = new Mock<ISecureXRepository>();
+            Mock<ISecureXRepository> MoqRepo = new Mock<ISecureXRepository>();
 
-        //    Account acc1 = new Account()
-        //    {
-        //        Id = 323,
-        //        Funds = 100m,
-        //        AccountType = "S"
-        //    };
+            Account acc1 = new Account()
+            {
+                Id = 323,
+                Funds = 100m,
+                AccountType = "S"
+            };
 
-        //    MoqRepo.Setup(x => x.AddAccount(acc1));
-        //    MoqRepo.Setup(x => x.Save());
-        //    MoqRepo.Setup(x => x.DeleteAccount(acc1.Id));
-        //    MoqRepo.Setup(x => x.Save());
+            MoqRepo.Setup(x => x.AddAccount(acc1));
+            MoqRepo.Setup(x => x.Save());
+            MoqRepo.Setup(x => x.DeleteAccount(acc1.Id));
+            MoqRepo.Setup(x => x.Save());
 
-        //    var con = new AccountController(MoqRepo.Object);
+            var con = new AccountController(MoqRepo.Object);
 
-        //    var result = await con.GetById(acc1.Id);
+            var result = await con.GetById(acc1.Id);
 
-        //    result.Value.Should().Be(null);
-        //}
+            result.Value.Should().Be(null);
+        }
 
     } 
 }
